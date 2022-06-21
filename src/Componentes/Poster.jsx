82cfb36a0ddc9f01b7fd958f19e32fc358f739f0
JimenaKatuMartinez <93katu@gmail.com>
2022-06-21 19:38:59 -0300

    arreglando tamaño posters para escritorio

--- src/Componentes/Poster.jsx
+++ src/Componentes/Poster.jsx
@@ -1,23 +1,31 @@
-import styled from "styled-components"
+import styled from "styled-components";
+
+const EnlacePelicula = styled.a`
+  @media screen and (min-width: 714px) {
+    width: 100%;
+  }
+`;
 
 const ImgPelicula = styled.img`
+  width: 100%;
+  aspect-ratio: 2/3;
+  object-fit: cover;
+  @media screen and (min-width: 714px) {
     width: 100%;
-    aspect-ratio: 2/3;
-    object-fit: cover;
-    @media screen and (min-width: 714px) {
-      width: 45%;
-    }
+  }
 
-    @media screen and (min-width: 1100px) {
-      width: 100%;
-    }
-`
-
+`;
 
 const Poster = (props) => {
   return (
-    <ImgPelicula src={`https://image.tmdb.org/t/p/original/${props.poster_path}`}/>
-  )
-}
+    <>
+      <EnlacePelicula href={`pelicula/${props.id}`}>
+        <ImgPelicula
+          src={`https://image.tmdb.org/t/p/original/${props.poster_path}`}
+        />
+      </EnlacePelicula>
+    </>
+  );
+};
 
-export default Poster
+export default Poster;
